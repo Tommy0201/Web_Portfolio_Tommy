30e9bf14af0e407eb408f00ffda14fa48884b428
Tommy0201 <ng.phucminh04@gmail.com>
2024-10-15 16:22:51 -0400

    modifying the text display

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -2,61 +2,63 @@
 const typedTextElement = document.getElementById('my-name');
 const textToType1 = 'Tommy Nguyen';
 const textToType2 = 'Villanova University';
-const textToType3 = "Computer Science and Statistics";
+const textToType3 = "Computer Science";
+const textToType4 = "Business Minor";
 
 // Function to simulate typing effect
 function typeText(text, index) {
   if (index < text.length) {
-    typedTextElement.innerHTML += `<span>${text.charAt(index)}</span>`;
-
+    typedTextElement.innerHTML += `${text.charAt(index)}`;
+    
     // Schedule the next character to be typed after a delay
     if (index === 0) {
-        setTimeout(() => {
-          typeText(text, index + 1);
-        }, 400); 
-      } else {
-        setTimeout(() => {
-          typeText(text, index + 1);
-        }, 70); 
+      setTimeout(() => {
+        typeText(text, index + 1);
+      }, 400);
+    } else {
+      setTimeout(() => {
+        typeText(text, index + 1);
+      }, 70);
+    }
   }
 }
-}
-function reTypeText(text,index) {
-    typedTextElement.textContent = ' ';
-    setTimeout(() => {
-        typeText(text, index);
-      }, 70); 
+
+function reTypeText(text, index) {
+  typedTextElement.textContent = ' ';
+  setTimeout(() => {
+    typeText(text, index);
+  }, 70);
 }
 
-function introTypeText(textToType1,textToType2,textToType3) {
-  typedTextElement.textContent=" ";
-setTimeout(() => {
-  typeText(textToType1,0);
-},50)
-    // typeText(textToType1,0);
-setTimeout(() => {
+function introTypeText(textToType1, textToType2, textToType3, textToType4) {
+  typedTextElement.textContent = " ";
+  setTimeout(() => {
+    typeText(textToType1, 0);
+  }, 50);
+  
+  setTimeout(() => {
     reTypeText(textToType2, 0);
   }, 2200);
-setTimeout(() => {
+  
+  setTimeout(() => {
     reTypeText(textToType3, 0);
   }, 5600);
-setTimeout(() => {
-    typedTextElement.textContent="Tommy Nguyen";
-  }, 12000);
+  
+  setTimeout(() => {
+    reTypeText(textToType4, 0);
+  }, 9000);
+  
+  setTimeout(() => {
+    typedTextElement.textContent = "Tommy Nguyen";
+  }, 13000);
 }
 
-
-introTypeText(textToType1,textToType2,textToType3)
-
+introTypeText(textToType1, textToType2, textToType3, textToType4);
 
 // Add a mouseover event listener to the element
 typedTextElement.addEventListener('mouseover', function() {
   // Check if the text content is "Tommy Nguyen"
   if (this.textContent === 'Tommy Nguyen') {
-    introTypeText(textToType1,textToType2,textToType3);
+    introTypeText(textToType1, textToType2, textToType3, textToType4);
   }
 });
-
-
-
-
