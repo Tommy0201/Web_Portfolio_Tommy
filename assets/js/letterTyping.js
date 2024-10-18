386411e4df212d3132d4f1283a6520d166f3601f
Tommy0201 <ng.phucminh04@gmail.com>
2024-10-18 02:39:44 -0400

    fixing the cursor

--- assets/js/letterTyping.js
+++ assets/js/letterTyping.js
@@ -7,51 +7,47 @@ const textToType4 = "Business Minor";
 
 // Function to simulate typing effect
 function typeText(text, index) {
-  if (index < text.length) {
-    typedTextElement.innerHTML += `${text.charAt(index)}`;
+  if (index <= text.length) {
+    typedTextElement.innerHTML = `${text.substring(0, index)}<span class="cursor">|</span>`;
     
     // Schedule the next character to be typed after a delay
-    if (index === 0) {
-      setTimeout(() => {
-        typeText(text, index + 1);
-      }, 400);
-    } else {
+    if (index < text.length) {
       setTimeout(() => {
         typeText(text, index + 1);
       }, 70);
-    }
+    }  
   }
 }
 
-function reTypeText(text, index) {
-  typedTextElement.textContent = ' ';
+// Function to clear and re-type the new text
+function reTypeText(text) {
+  typedTextElement.innerHTML = '<span class="cursor">|</span>';
   setTimeout(() => {
-    typeText(text, index);
-  }, 70);
+    typeText(text, 0);
+  }, 500);
 }
 
-function introTypeText(textToType1, textToType2, textToType3, textToType4) {
-  typedTextElement.textContent = " ";
-  setTimeout(() => {
-    typeText(textToType1, 0);
-  }, 50);
+
+// Function to handle the entire sequence
+function introTypeText() {
+  typeText(textToType1, 0); // First text
   
   setTimeout(() => {
-    reTypeText(textToType2, 0);
+    reTypeText(textToType2); // Second text
   }, 2200);
   
   setTimeout(() => {
-    reTypeText(textToType3, 0);
+    reTypeText(textToType3); // Third text
   }, 5600);
   
   setTimeout(() => {
-    reTypeText(textToType4, 0);
+    reTypeText(textToType4); // Fourth text
   }, 9000);
   
   setTimeout(() => {
-    typedTextElement.textContent = "Tommy Nguyen";
+    typedTextElement.innerHTML = `Tommy Nguyen`; // Final display
   }, 13000);
 }
 
-introTypeText(textToType1, textToType2, textToType3, textToType4);
+introTypeText();
 
