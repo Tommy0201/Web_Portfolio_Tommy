5c4e72761e48c02c3edf3c682141ab4d8b567ced
Tommy0201 <ng.phucminh04@gmail.com>
2024-10-18 05:00:17 -0400

    fixing css, layout, adding in pics

--- assets/js/education-swipes.js
+++ assets/js/education-swipes.js
@@ -4,33 +4,35 @@ const educationLevels = [
         gpa: "GPA: 3.96 / 4.0",
         honors: "Honors Student",
         scholarship: " Tapia 2024 Conference VU Scholar, Veritas Scholar 2026,  Connelly-Delouvrier Scholar 2025",
-        description: "Hi, I am Minh (Tommy) Nguyen, a Computer Science student at Villanova University with a passion for AI/ML research and full-stack development. I’ve had the opportunity to work on exciting projects like developing AI-powered voicebots in my internship and fine-tuning large language models for low-resource languages in my research. I love the challenge of creating innovative solutions and am always eager to learn. Currently, as the Badminton Club President, I enjoy leading others to compete in badminton championships. I’m actively seeking a software engineering internship or data science internship for Summer 2025 to continue growing my expertise and making an impact.",
+        description: "Hi, I am Minh (Tommy) Nguyen, a Computer Science student with a passion for AI/ML research and full-stack development. I’ve had the opportunity to work on exciting projects like developing AI-powered voicebots in my internship and fine-tuning large language models for low-resource languages in my research. I love the challenge of creating innovative solutions and am always eager to learn. Currently, as the Badminton Club President, I enjoy leading others to compete in badminton championships. I’m actively seeking a software engineering internship or data science internship for Summer 2025 to continue growing my expertise and making an impact.",
         color: "#0f437c", 
-        image: "images/VU_Logo.jpg", 
+        image: "images/vu2.jpg", 
         school: "Villanova University",
     },
     {
         major: "Specialization: Physics",
-        gpa: "GPA: 9.8 / 10",
-        honors: "Silver medal",
-        scholarship: "Merit Scholarship",
-        description: "Engaged in various science fairs and projects. Participated in coding competitions.",
-        color: "rgb(66 66 66)",
+        gpa: "GPA: 9.8 / 10 ",
+        honors: "AP Computer Science A: 5; AP Calculus BC: 5",
+        scholarship: "AI-JAM Silicon Valley 2021 Gold, International Young Scientists Innovation Exhibition 2021 Gold, Asian Science and Mathematics Olympiad 2021 1st Runner up, Singapore Asian Schools Math Olympiad 2020 Bronze",
+        description: " I attended the top one highschool in Vietnam with acceptance rate of 10%. During this time, I won multiple awards related to Math and Sciences. I was also the President of an astronomy organization where I developed content, hosted courses, and engaged hundreds of students. Additionally, I took pride in leading various community initiatives, such as teaching English to underprivileged children and organizing school-wide events.",
+        color: "#517fa8",
+
         image: "images/pic05.jpg", 
         school: "Hanoi-Amsterdam Highschool For the Gifted",
 
     },
-    {
-        major:"",
-        gpa: "GPA: 9.2 / 10",
-        honors: "Graduated with Distinction",
-        scholarship: "Scholarship Recipient",
-        description: "Focused on foundational studies, excelling in mathematics and science.",
-        color: "#517fa8",
-        image: "images/pic05.jpg", 
-        school: "Hanoi-Amsterdam Middle School For the Gifted",
+    // {
+    //     major:"",
+    //     gpa: "GPA: 9.2 / 10",
+    //     honors: "Graduated with in top 1 student cohort in Math in my middle school",
+    //     scholarship: "",
+    //     description: "I attended the top one secondary school in Vietnam with acceptance rate of < 10%. During middle school, I competed and won various Math and Physics competition.",
+    //     color: "rgb(66 66 66)",
 
-    }
+    //     image: "images/pic05.jpg", 
+    //     school: "Hanoi-Amsterdam Middle School For the Gifted",
+
+    // }
 ];
 
 let currentEducationIndex = 0;
